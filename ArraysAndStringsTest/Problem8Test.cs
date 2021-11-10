e71458c295a8d0c7f01849a7dd38f50571485177
Juan E Afable <juanjuanzerojuan@gmail.com>
2021-11-10 15:34:09 -0500

    finished problem 8

---
+++ ArraysAndStringsTest/Problem8Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArraysAndStrringsProblems.Problem8;
+using Xunit;
+
+namespace ArraysAndStringsTest
+{
+    
+    public class Problem8Test
+    {
+        [Fact]
+        public void Test0()
+        {
+            var loL = new List<List<int>>()
+            {
+                new List<int>(){1,2,3,4,5},
+                new List<int>(){1,2,3,4,5},
+                new List<int>(){1,2,0,4,5},
+                new List<int>(){1,2,3,4,5},
+                new List<int>(){1,2,3,4,5}
+            };
+
+            var rotated = Problem8Solution.ZeroMatrix(loL);
+
+            var rotatedList = new List<List<int>>()
+            {
+                new List<int>(){1,2,0,4,5},
+                new List<int>(){1,2,0,4,5},
+                new List<int>(){0,0,0,0,0},
+                new List<int>(){1,2,0,4,5},
+                new List<int>(){1,2,0,4,5}
+            };
+
+            Assert.Equal(rotatedList, rotated);
+        }
+    }
+}
--- ArraysAndStrringsProblems/Problem1.8/Problem8Solution.cs
+++ ArraysAndStrringsProblems/Problem1.8/Problem8Solution.cs
@@ -8,7 +8,48 @@ namespace ArraysAndStrringsProblems.Problem8
 {
     public static class Problem8Solution
     {
+        public static List<List<int>> ZeroMatrix(List<List<int>> matrix)
+        {
+            bool[] zeroRows = new bool[matrix.Count];
+            bool[] zeroCols = new bool[matrix[0].Count];
 
+            //loop through the rows of each array
+            for (int row = 0; row < matrix.Count; row++)
+            {
+                for (int col = 0; col < matrix[row].Count; col++)
+                {
+                    //if you encounter a zero set the row to true and set the col to true
+                    if(matrix[row][col] == 0)
+                    {
+                        zeroRows[row] = true;
+                        zeroCols[col] = true;
+                    }
+                }
+            }
+
+            //then just loop through the zeroRows and cols again to create the zeroes
+            for (int row = 0; row < matrix.Count; row++)
+            {
+
+                //loop through the cols and decide when to use
+                for (int col = 0; col < matrix[row].Count; col++)
+                {
+                    if (zeroRows[row])
+                    {
+                        matrix[row][col] = 0;
+                    }
+                    else
+                    {
+                        if (zeroCols[col])
+                        {
+                            matrix[row][col] = 0;
+                        }
+                    }
+                }
+            }
+
+            return matrix;
+        }
     }
 
 
